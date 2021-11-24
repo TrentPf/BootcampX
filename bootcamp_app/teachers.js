f0021a47f44bfdbeb6854a0ed68dc469e2e85832
Trent Pfeiffer <trentpf@hotmail.com>
2021-11-24 12:07:18 -0500

    Add app functionality for database

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const args = process.argv;
+let input = args.slice(2);
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${input[0] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort} : ${user.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
